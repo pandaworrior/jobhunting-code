ef26778587f131d2395644d55683ef19fce1e2a2
chengli <chenglinkcs@gmail.com>
2015-02-15 23:41:50 +0100

    implemented the selection sort

--- src/sort/allsort.java
+++ src/sort/allsort.java
@@ -32,6 +32,38 @@ public class allsort {
 		return unsortedA;
 	}
 	
+	/*
+	 * Selection_sort initially scans an array from 0 to n
+	 * to identify the minimal element i, and swap the ith element
+	 * and the first element in the array. The following iteration will
+	 * start by scanning from 1 to n.
+	 */
+	static int[] selection_sort(int[] unsortedA) throws Exception{
+		if(unsortedA == null){
+			throw new Exception(ARRAY_NULL_EXP_STR);
+		}
+		
+		for(int i = 0; i < unsortedA.length-1; i++){
+			
+			int minValue = unsortedA[i];
+			int minIndex = i;
+			for(int j = i + 1; j < unsortedA.length; j++){
+				if(minValue > unsortedA[j]){
+					minValue = unsortedA[j];
+					minIndex = j;
+				}
+			}
+			
+			if(minIndex != i){
+				int temp = unsortedA[i];
+				unsortedA[i] = unsortedA[minIndex];
+				unsortedA[minIndex] = temp;
+			}
+		}
+		
+		return unsortedA;
+	}
+	
 	static void print_array(int[] arr) throws Exception{
 		if(arr == null){
 			throw new Exception(ARRAY_NULL_EXP_STR);
@@ -49,12 +81,23 @@ public class allsort {
 	public static void main(String args[]){
 		
 		int[] unsortedArray = {3,1,9,10,37,28,15,17,22,0,100,53,55,53,44};
+		int[] sortedArray;
 		
 		//call bubble sort
 		try {
-			int[] sortedArrary = bubble_sort(unsortedArray);
-			print_array(sortedArrary);
+			sortedArray = bubble_sort(unsortedArray);
+			print_array(sortedArray);
 		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		//call selection sort
+		
+		try {
+			sortedArray = selection_sort(unsortedArray);
+			print_array(sortedArray);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
